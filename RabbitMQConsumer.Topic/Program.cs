3c49537654d1a8904b98ec892286d47cc6d1eab5
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-09-06 18:12:05 +0430

    add consumer of topic exchange

---
+++ RabbitMQConsumer.Topic/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQConsumer.Topic
+{
+    class Program
+    {
+        private const string UName = "guest";
+        private const string Pwd = "guest";
+        private const string HName = "localhost";
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = HName,
+                UserName = UName,
+                Password = Pwd,
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            // accept only one unack-ed message at a time
+
+            channel.BasicQos(0, 1, false);
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            channel.BasicConsume("demoqueueTopic", false, messageReceiver);
+            Console.ReadLine();
+
+        }
+
+    }
+}
