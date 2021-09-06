daa8c62749d66323d04abec815534083e9b5ce84
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-09-06 17:09:39 +0430

    add some description

--- RequestRabbitMQ/Program.cs
+++ RequestRabbitMQ/Program.cs
@@ -20,21 +20,23 @@ namespace RequestRabbitMQ
 
             var connection = connectionFactory.CreateConnection();
             var model = connection.CreateModel();
+
+            //1.Crate Exchange 
             //model.ExchangeDeclare("demoExchange", ExchangeType.Direct);
             //Console.WriteLine("Creating Exchange");
 
+            //2.Crate Queue 
             //model.QueueDeclare("demoqueue", true,false,false,null);
             //Console.WriteLine("Creating Queue");
 
+            //3.Bind created queue to existing exchange 
             //model.QueueBind("demoqueue", "demoExchange", "dirextexchange_key");
             //Console.WriteLine("Creating Binding");
 
+            //4.Publish message
             var properties = model.CreateBasicProperties();
             properties.Persistent = false;
-
             byte[] messageBuffer = Encoding.Default.GetBytes("Direct message ...");
-
-
             model.BasicPublish("demoExchange", "dirextexchange_key", properties, messageBuffer);
             Console.WriteLine("Message sent");
 
