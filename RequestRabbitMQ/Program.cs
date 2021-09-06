d1bb74f5c5b99c4919976ae0fffcb0f236123ec1
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-09-06 17:05:15 +0430

    Add project files.

---
+++ RequestRabbitMQ/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RequestRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "guest";
+            string password = "guest";
+            string hostName = "localhost";
+            var connectionFactory = new ConnectionFactory()
+            {
+                UserName = userName,
+                Password = password,
+                HostName = hostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+            //model.ExchangeDeclare("demoExchange", ExchangeType.Direct);
+            //Console.WriteLine("Creating Exchange");
+
+            //model.QueueDeclare("demoqueue", true,false,false,null);
+            //Console.WriteLine("Creating Queue");
+
+            //model.QueueBind("demoqueue", "demoExchange", "dirextexchange_key");
+            //Console.WriteLine("Creating Binding");
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            byte[] messageBuffer = Encoding.Default.GetBytes("Direct message ...");
+
+
+            model.BasicPublish("demoExchange", "dirextexchange_key", properties, messageBuffer);
+            Console.WriteLine("Message sent");
+
+            Console.ReadLine();
+
+        }
+    }
+}
